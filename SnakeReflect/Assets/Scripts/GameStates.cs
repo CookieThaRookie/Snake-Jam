dc02f3c8f59c9d30b1dd8849f35f98d9e8358511
CookieThaRookie <30510722+CookieThaRookie@users.noreply.github.com>
2019-07-03 20:13:05 +0200

    Map + Gamestates
    readying this for a merge

---
+++ SnakeReflect/Assets/Scripts/GameStates.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+    public GameObject victoryText;
+    public GameObject defeatText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "End")
+        {
+            print("Entered End trigger");
+            victoryText.SetActive(true);
+        }
+
+        if(collision.gameObject.tag == "Bullet")
+        {
+            print("Entered Bullet trigger");
+            victoryText.SetActive(true);
+        }
+    }
+
+}
