c75ed808b47af41d4b6f806afa5bb3368a84adc7
patrickholt <patrick1995holt@gmail.com>
2019-10-11 20:40:26 +0200

    Add snake player
    Player can now move the snake around and have the tail rendered behind. It will render forever and not "follow" yet.

---
+++ SnakeReflect/Assets/Tail.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Tail : MonoBehaviour
+{
+    public float pointSpacing = .1f; // How far to move before we fraw new point
+    public Transform head;
+
+    List<Vector2> points;
+
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+
+        points = new List<Vector2>();
+
+        SetPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We only want to set a new point if we have moved far enough away
+        if (Vector3.Distance(points.Last(), head.position) > pointSpacing)
+            SetPoint();
+    }
+
+    void SetPoint()
+    {
+        points.Add(head.position);
+
+        line.positionCount = points.Count;
+        line.SetPosition(points.Count - 1, head.position);
+    }
+}
