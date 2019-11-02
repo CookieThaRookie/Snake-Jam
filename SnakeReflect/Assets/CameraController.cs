b9932de62d522a6269830765c75c2dbc798203f3
patrickholt <patrick1995holt@gmail.com>
2019-11-02 15:32:33 +0100

    Add basic camera follow
    Simple script added to get the camera to follow the player

---
+++ SnakeReflect/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // Object to focus on
+    public float speed; // How fast should the camera move?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
